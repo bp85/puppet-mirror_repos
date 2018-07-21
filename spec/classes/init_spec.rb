031363575703d2048ee5e8a19d104108ddf52659
bhanu prasad <bhanu_prasad@hms.harvard.edu>
2018-07-21 00:05:42 -0400

    spec_2

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 describe 'mirror_repos' do
-  context 'with default values for all parameters' do
-    let(:title) { 'mirror_repos' }
-    it { is_expected.to compile }
-    it { is_expected.to compile.with_all_deps }
-    let(:facts) { { osfamily: 'RedHat', operatingsystemmajrelease: '6' } }
-    let(:facts) { { osfamily: 'RedHat', operatingsystemmajrelease: '7' } }
+  let(:title) { 'mirror_repos' }
+
+  on_supported_os.each do |os, facts|
+    context "with default values for all parameters on #{os}" do
+      let(:facts) do
+        facts
+      end
+      it { is_expected.to compile.with_all_deps }
+    end
   end
 end
