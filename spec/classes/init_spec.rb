38dd54ec7abf6cc9e21a61cad0693f0331182a60
bhanu prasad <bhanu_prasad@hms.harvard.edu>
2018-07-21 00:01:21 -0400

    spec_

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 describe 'mirror_repos' do
   context 'with default values for all parameters' do
-    it { is_expected.to contain_class('mirror_repos') }
+    let(:title) { 'mirror_repos' }
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
     let(:facts) { { osfamily: 'RedHat', operatingsystemmajrelease: '6' } }
     let(:facts) { { osfamily: 'RedHat', operatingsystemmajrelease: '7' } }
   end
