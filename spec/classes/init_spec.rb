437ec98be3a4ee6c3edaf50c732533e1a920d4e1
bhanu prasad <bhanu_prasad@hms.harvard.edu>
2018-07-21 00:30:52 -0400

    add class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,6 +8,10 @@ describe 'mirror_repos' do
         facts
       end
 
+      it { is_expected.to contain_class('mirror_repos::apache') }
+      it { is_expected.to contain_class('mirror_repos::params') }
+      it { is_expected.to contain_class('mirror_repos::install') }
+      it { is_expected.to contain_class('mirror_repos::config') }
       it { is_expected.to compile.with_all_deps }
     end
   end
